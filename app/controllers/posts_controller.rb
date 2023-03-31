22582751a1f080ace267f77f2d826b59d4626712
jefferson.pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-03-31 17:58:38 +0800

    refactor post applied DRY

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,8 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
-  end
-
-  def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -12,7 +10,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params[:post].permit(:title, :address, :content))
+    @post = Post.new(post_params)
     if @post.save
       redirect_to posts_path
     else
@@ -20,13 +18,12 @@ class PostsController < ApplicationController
     end
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+  def show; end
+
+  def edit; end
   
   def update
-     @post = Post.find(params[:id])
-     if @post.update(params.require(:post).permit(:title, :content, :address))
+     if @post.update(post_params)
        redirect_to post_path(@post)
      else
        render :edit, status: :unprocessable_entity
@@ -34,8 +31,18 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :address)
+  end
+
 end
