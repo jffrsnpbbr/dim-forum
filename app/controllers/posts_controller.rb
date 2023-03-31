d76fe517aabf33b43a254a801a4f5cd9b1bcb48b
jefferson.pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-03-31 16:58:29 +0800

    add model post validation, and new message error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,17 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post].permit(:title, :address, :content))
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
