59fdc4c3b692eff92dcbbbd5627115aed1dab553
jefferson.pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-03-31 14:36:53 +0800

    Add model post and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_062307) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.text "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -28,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 0) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
