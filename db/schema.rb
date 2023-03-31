c1f23fd696011c7ea43e0a4c4966ca0c428e7fb8
jefferson.pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-03-31 14:06:55 +0800

    Add model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_31_053744) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_055357) do
+  create_table "categories", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "content"
