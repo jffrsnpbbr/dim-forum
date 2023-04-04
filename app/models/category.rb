64ff43649379a1658055d96fbe9feba37b30effc
jefferson.pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-04-04 17:10:25 +0800

    add softkil to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,12 @@
 class Category < ApplicationRecord
+  default_scope { where(deleted_at: nil) }
+
+  validates :name, presence: true
+  
   has_many :post_category_ships
   has_many :posts, through: :post_category_ships
-  
-  validates :name, presence: true
+
+  def destroy
+    update(deleted_at: Time.current)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_085924) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_04_090507) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
   end
 
   create_table "comments", charset: "utf8mb4", force: :cascade do |t|
