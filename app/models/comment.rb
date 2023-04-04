04c398d445f130cf4b2f19d3da7c3a2e000e084c
jefferson.pababero <93456420+jffrsnpbbr@users.noreply.github.com>
2023-04-04 16:59:45 +0800

    add softkill comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ApplicationRecord
+  default_scope { where(deleted_at: nil) }
+
   validates :content, presence: true
 
   belongs_to :post
+
+  def destroy
+    update(deleted_at: Time.current)
+  end
 end
