59e8b59be1beaed9f8f9377d6e01b8df87d297fe
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-04-06 18:36:58 +0800

    add post comment_count, top 3 post show hot tag
    Root path order by comments count, and top three post show a tag "HOT"

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
 
   validates :content, presence: true
 
-  belongs_to :post
+  belongs_to :post, counter_cache: true
   belongs_to :user
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_05_130420) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_093550) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_05_130420) do
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
     t.bigint "user_id"
+    t.integer "comments_count"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
