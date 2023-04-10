b139ff17a5c7b74130d4b9ccef5460c8d7dbdf0c
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-04-11 07:24:14 +0800

    add column post_id to post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_06_093550) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_10_170736) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_093550) do
     t.datetime "deleted_at"
     t.bigint "user_id"
     t.integer "comments_count"
+    t.string "post_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
